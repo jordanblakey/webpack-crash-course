a230b95299cf1afdc5989089b5f0173db675bedd
Jordan Blakey <poshski1@gmail.com>
2017-05-18 22:36:55 -0500

    bundling js and css, and npm modules using style-loader and css-loader modules in Webpack

---
+++ people.js
@@ -0,0 +1,11 @@
+// function getHello(){
+//     return 'Hello there.';
+// }
+
+let people = [
+    {name: 'John Doe'},
+    {name: 'Steve Smith'},
+    {name: 'Carol Williams'}
+];
+
+module.exports = people;
